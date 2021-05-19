364147fa135cc87922629f562daf40880739ac25
Gravitcapa-py <gravitcapa.py@gmail.com>
2021-05-19 21:10:44 +0300

    5step

--- brain_games/scripts/brain_games1.py
+++ brain_games/scripts/brain_games1.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from brain_games.scripts.brain_even import is_even
 from brain_games import cli
 
 
 def main():
     print('Welcome to the Brain Games!')
-    cli.welcome_user()
+    name = cli.welcome_user()
+    is_even(name)
 
 
 if __name__ == '__main__':
